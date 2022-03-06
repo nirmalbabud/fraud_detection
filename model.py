fa96c18d7649f1d0af40fa58a46e7daf9e21fa23
nirmalbabud <nirmalbabud@gmail.com>
2022-03-06 11:45:29 +0530

    added best model

--- model.py
+++ model.py
@@ -1,2 +1,13 @@
 
 print("all model codes")
+
+def model1():
+    print("this is model1")
+
+def model2():
+    print("this is model2"
+
+def model3():
+    print("this is model3")
+
+print("model2 is the best model")
